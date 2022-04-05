37bb1bc2944c23a8209c7c06089e593a82bbc086
Perumallpardhiv <96647299+Perumallpardhiv@users.noreply.github.com>
2022-04-05 18:06:41 +0530

    Update Navbar.jsx

--- mernweb/src/components/Navbar.jsx
+++ mernweb/src/components/Navbar.jsx
@@ -6,15 +6,15 @@ import { NavLink } from 'react-router-dom';
 const Navbar = (props) => {
     return (
         <div>
-            <nav className="navbar navbar-expand-lg navbar-light shadow">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top" >
   <div className="container">
    
     
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+    <div className="collapse navbar-collapse" id="navbarSupportedContent" >
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
      
         <li className="nav-item">
           <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
@@ -35,7 +35,6 @@ const Navbar = (props) => {
           <NavLink className="nav-link" to="/Testimonials">Founders</NavLink>
         </li>
       </ul>
-
 
       {props.auth ?
       <>
@@ -59,7 +58,4 @@ const Navbar = (props) => {
         </div>
     );
 }
-
-
-
 export default Navbar;
